16048a16bd25dc43d88213668caa40d0c878be79
Ross Mawdsley <48100198+rossmawd@users.noreply.github.com>
2023-09-13 10:18:30 +0100

    added tailwind config and directives

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
+// 'screens': screen sizes used by Dawn theme
+
 module.exports = {
-  content: [],
+  prefix: 'x',
+  content: [
+    './layout/*.liquid',
+    './templates/*.liquid',
+    './templates/customers/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+  ],
   theme: {
-    extend: {},
+    screens: {
+      md: '750px',
+      lg: '990px',
+    },
+    extend: {
+      fontFamily: {
+        heading: 'var(--font-heading-family)',
+      },
+    },
   },
   plugins: [],
-}
-
+};
